abd000d8106cbfc59ff3ea380c6830785ac83f98
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2023-07-12 01:43:05 +0700

    feat: add 'ghost' variant button

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -3,12 +3,16 @@ import { forwardRef } from "react";
 import { cn, cva, VariantProps } from "@/lib/styles";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-md font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  [
+    "inline-flex items-center justify-center rounded-md text-md font-medium transition-colors",
+    "focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  ],
   {
     variants: {
       variant: {
         primary: "bg-primary text-primary-foreground",
         outline: "border border-primary text-primary",
+        ghost: "bg-transparent text-black",
       },
       size: {
         lg: "h-10 px-4 py-2.5",

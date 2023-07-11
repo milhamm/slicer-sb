bac23f6d1601b19dff10b4708d9163fc1710bbac
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2023-07-12 01:42:46 +0700

    feat: add `ui` entrypoint

---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export * from "./Badge";
+export * from "./Breadcrumbs";
+export * from "./Button";
+export * from "./Card";
+export * from "./Container";
+export * from "./Space";
+export * from "./Title";

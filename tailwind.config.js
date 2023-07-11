6f54e866564ffb3192696afae20b4e52763ef9eb
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2023-07-11 19:57:57 +0700

    refactor: tailwind color to nested

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,13 @@ module.exports = {
     extend: {
       colors: {
         primary: "rgb(var(--color-primary) / <alpha-value>)",
-        ["primary-foreground"]:
+        "primary-foreground":
           "rgb(var(--color-primary-foreground) / <alpha-value>)",
+        grey: {
+          100: "rgba(241, 241, 241, 1)",
+          200: "rgba(230, 230, 230, 1)",
+          600: "rgba(128, 128, 128, 1)",
+        },
       },
     },
   },

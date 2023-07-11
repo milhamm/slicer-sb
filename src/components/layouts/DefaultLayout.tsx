df6078902ee2cb7e7632ed3a3e2e5b31667d1043
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2023-07-12 01:44:34 +0700

    feat: add `DefaultLayout` layout component

---
+++ src/components/layouts/DefaultLayout.tsx
@@ -0,0 +1,30 @@
+import { Navbar } from "@/components/Navbar";
+import { Breadcrumbs } from "@/components/ui/Breadcrumbs";
+import { Button, Space, containerVariants } from "@/components/ui";
+import { cn } from "@/lib/styles";
+
+type DefaultLayoutProps = {
+  children: React.ReactNode;
+};
+
+const DefaultLayout = ({ children }: DefaultLayoutProps) => {
+  return (
+    <>
+      <Navbar className={cn(containerVariants())}>
+        <Navbar.Logo src="/sobat-bangun.svg" alt="Sobat Bangun Logo" />
+        <Space>
+          <Button variant="ghost">Daftar</Button>
+          <Button>Masuk</Button>
+        </Space>
+      </Navbar>
+      <Breadcrumbs className={cn("my-5", containerVariants())}>
+        <Breadcrumbs.Item href="/">Home</Breadcrumbs.Item>
+        <Breadcrumbs.Item href="/designs">Layanan Desain</Breadcrumbs.Item>
+        <Breadcrumbs.Item href="#">Omah Apik 3</Breadcrumbs.Item>
+      </Breadcrumbs>
+      <main>{children}</main>
+    </>
+  );
+};
+
+export { DefaultLayout };

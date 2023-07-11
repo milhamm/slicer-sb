a746ae4d6173237aa72220dc7997640558b32a74
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2023-07-12 00:09:37 +0700

    feat: add `Breadcrumbs` component

---
+++ src/components/icons/ChevronRight.tsx
@@ -0,0 +1,20 @@
+const ChevronRight = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <g clip-path="url(#clip0_6_1241)">
+        <path
+          d="M10 6L8.59003 7.41L13.17 12L8.59003 16.59L10 18L16 12L10 6Z"
+          fill="#C5C7D0"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export { ChevronRight };
---
+++ src/components/ui/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,63 @@
+import React, {
+  ComponentProps,
+  ReactNode,
+  createContext,
+  forwardRef,
+  useContext,
+} from "react";
+import Link from "next/link";
+
+import { ChevronRight } from "@/components/icons";
+import { cn } from "@/lib/styles";
+
+const BreadcrumbsContext = createContext<ReactNode>(null);
+
+export type BreadcrumbsProps = {
+  separator?: React.ReactNode;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+const BreadcrumbsContainer = forwardRef<HTMLDivElement, BreadcrumbsProps>(
+  ({ separator = <ChevronRight />, children, className, ...props }) => {
+    return (
+      <BreadcrumbsContext.Provider value={separator}>
+        <div className={cn("flex gap-3", className)} {...props}>
+          {children}
+        </div>
+      </BreadcrumbsContext.Provider>
+    );
+  }
+);
+
+BreadcrumbsContainer.displayName = "Breadcrumbs";
+
+export type BreadcrumbsItemProps = ComponentProps<typeof Link>;
+
+const BreadcrumbsItem = forwardRef<HTMLAnchorElement, BreadcrumbsItemProps>(
+  ({ children, className, ...props }, ref) => {
+    const separator = useContext(BreadcrumbsContext);
+    return (
+      <>
+        <Link
+          ref={ref}
+          className={cn(
+            "font-medium text-primary",
+            "hover:underline last-of-type:pointer-events-none last-of-type:text-grey-600",
+            className
+          )}
+          {...props}
+        >
+          {children}
+        </Link>
+        <div className={cn("last-of-type:hidden")}>{separator}</div>
+      </>
+    );
+  }
+);
+
+BreadcrumbsItem.displayName = "BreadcrumbsItem";
+
+const Breadcrumbs = Object.assign(BreadcrumbsContainer, {
+  Item: BreadcrumbsItem,
+});
+
+export { Breadcrumbs };
